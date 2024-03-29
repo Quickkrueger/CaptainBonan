95b3ad1f65441e45388ae609352115883be677d2
Marshall Krueger <mgkrueger4@gmail.com>
2024-03-29 14:02:01 -0400

    Working on blob

--- Assets/Characters/Scripts/EnemyController.cs
+++ Assets/Characters/Scripts/EnemyController.cs
@@ -9,6 +9,10 @@ public class EnemyController : MonoBehaviour
     AnimationControl animControl;
     NavmeshAgentControl navmeshAgentControl;
     private GameObject target;
+    [Range(.1f, 2f)]
+    public float attackSpeed;
+
+    Coroutine attackRoutine;
     // Start is called before the first frame update
     void Start()
     {
@@ -38,16 +42,26 @@ public class EnemyController : MonoBehaviour
 
     private void Move(float speed)
     {
+        if (attackRoutine != null & speed > 0.3f)
+        {
+            StopCoroutine(attackRoutine);
+            attackRoutine = null;
+        }
         animControl.UpdateFloatProperty("Speed", speed);
     }
 
     private void Attack()
     {
-
+        if (attackRoutine == null)
+        {
+            attackRoutine = StartCoroutine(AttackRoutine(new WaitForSeconds(attackSpeed)));
+        }
     }
 
     IEnumerator AttackRoutine(WaitForSeconds seconds)
     {
+        animControl.UpdateTriggerProperty("Attack");
         yield return seconds;
+        attackRoutine = StartCoroutine(AttackRoutine(seconds));
     }
 }
--- Assets/Characters/Scripts/NavmeshAgentControl.cs
+++ Assets/Characters/Scripts/NavmeshAgentControl.cs
@@ -8,7 +8,6 @@ using UnityEngine.AI;
 using UnityEngine.Events;
 using UnityEngine.InputSystem.Controls;
 [RequireComponent(typeof(NavMeshAgent))]
-[RequireComponent (typeof(AnimationControl))]
 public class NavmeshAgentControl : MonoBehaviour
 {
     [SerializeField]
@@ -23,19 +22,26 @@ public class NavmeshAgentControl : MonoBehaviour
     [HideInInspector]
     public UnityAction<float> MoveAction;
 
+    Coroutine followRoutine;
+
     private void Awake()
     {
         agent = GetComponent<NavMeshAgent>();
+        agent.stoppingDistance = stopDistance - (stopDistance * 0.1f);
     }
 
     public void BeginFollow(Transform newTarget)
     {
             target = newTarget;
-            StartCoroutine(FollowTargetRoutine(new WaitForFixedUpdate()));
+        if (followRoutine == null)
+        {
+            followRoutine = StartCoroutine(FollowTargetRoutine(new WaitForFixedUpdate()));
+        }
     }
 
     private IEnumerator FollowTargetRoutine(WaitForFixedUpdate waitForFixedUpdate)
     {
+
         MoveAction.Invoke(agent.velocity.magnitude / agent.speed);
         agent.SetDestination(target.transform.position);
 
@@ -44,15 +50,16 @@ public class NavmeshAgentControl : MonoBehaviour
             target = null;
             agent.ResetPath();
             MoveAction.Invoke(0);
-            StopAllCoroutines();
+            StopCoroutine(followRoutine);
+            followRoutine = null;
         }
-        else if (Vector3.Distance(target.position, transform.position) <= stopDistance - (stopDistance / 4))
+        else if (Vector3.Distance(target.position, transform.position) <= stopDistance)
         {
             StopAction.Invoke();
         }
 
         yield return waitForFixedUpdate;
-        StartCoroutine(FollowTargetRoutine(waitForFixedUpdate));
+        followRoutine = StartCoroutine(FollowTargetRoutine(waitForFixedUpdate));
     }
 
 
