22846bf9537401e8dc8fe8d15e7528dab4684f6c
Marshall Krueger <mgkrueger4@gmail.com>
2024-03-30 18:55:54 -0400

    Update EnemyController.cs

--- Assets/Characters/Scripts/EnemyController.cs
+++ Assets/Characters/Scripts/EnemyController.cs
@@ -85,6 +85,7 @@ public class EnemyController : MonoBehaviour
         {
             animControl.UpdateTriggerProperty("Die");
             deathEvent.Invoke();
+            StopAllCoroutines();
             this.enabled = false;
         }
         
